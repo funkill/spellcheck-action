156df38544b36bbd026f3c2af11a6f685833ac6a
funkill2 <funkill2@gmail.com>
2019-11-16 13:58:40 +0300

    changed checking method

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,12 @@ sources=${GITHUB_WORKSPACE}/${INPUT_SOURCES}
 files_pattern=${INPUT_FILES_PATTERN}
 lang=${INPUT_LANG}
 
-
+echo $dict_filename $sources $files_pattern $lang
 if [ ! -f $dict_filename ]; then
   echo "personal_ws-1.1 $lang 0 utf-8" > "$dict_filename"
 fi
 # aspell repeatedly modifies the personal dictionary for some reason,
 # so we should use a copy of our dictionary.
-find $sources -type f -name $files_pattern -exec aspell --lang $lang --ignore 3 --dont-backup --personal="$dict_filename" check {} \;
+find $sources -type f -name $files_pattern -exec aspell --lang $lang --ignore 3 --dont-backup --personal="$dict_filename" list {} \;
 
 #
