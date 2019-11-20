a3ae249b2bcd2ac9ecc25b83705f738bfe74f8c1
funkill2 <funkill2@gmail.com>
2019-11-20 21:44:35 +0300

    fixed showing errors in files and exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,18 +12,24 @@ if [ ! -f $dict_filename ]; then
   echo "personal_ws-1.1 ${lang} 0 utf-8" > "${dict_filename}"
 fi
 
-if [[ "${only_changed}" == "true" ]]; then
+if [ "${only_changed}" = "true" ]; then
   files=$(git show -m --name-only -1 --format="format:" | grep --color=never -i "$files_pattern")
 else
   files=$(find "${GITHUB_WORKSPACE}/${sources}" -type f -name "${files_pattern}")
 fi
 
+failed=0
 for file in ${files}; do
-  echo "${file}:"
-  aspell --lang $lang --ignore 3 --dont-backup --personal="${dict_filename}" list < "${file}" |
-    while read word; do
+  words=$(aspell --lang $lang --ignore 3 --dont-backup --personal="${dict_filename}" list < "${file}")
+  if [ "$words" != "" ]; then
+    failed=1
+    echo "${file}:"
+    for word in $words; do
       grep -on "${word}" "${file}"
     done
+  fi
 done
+
+exit $failed
 
 #
